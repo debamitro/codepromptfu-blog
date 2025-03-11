4328a309958a0efa61d7a076003959c8442cd447
Debamitro Chakraborti <debamitro@gmail.com>
2025-03-11 13:08:39 -0400

    added tag cloud, turned google analytics back on

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./themes/tailwind-theme/layouts/**/*.html"],
+  content: [
+    "./themes/tailwind-theme/layouts/**/*.html",
+    "./layouts/**/*.html"
+  ],
   theme: {
     extend: {},
   },
